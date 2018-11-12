3e3a34f7f035819b666d586dc621e71976352b98
kahartman2 <41496627+kahartman2@users.noreply.github.com>
2018-11-12 11:23:23 -0500

    runs infatuation_updated file

---
+++ infatuation_cleaner.py
@@ -0,0 +1,9 @@
+from infatuation_updated import *
+
+restaurant_output = ListingBuilder()
+full_list = restaurant_output.run()
+print(full_list)
+
+# for i in full_list:
+#     if i['Name'] == 'abcV':
+#         print(i)
