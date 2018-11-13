a29c3ad29f31c51bb4e711da10d4e42ead1cd93f
kahartman2 <41496627+kahartman2@users.noreply.github.com>
2018-11-13 12:52:33 -0500

    updated name list

--- comprehensive_name.py
+++ comprehensive_name.py
@@ -1,17 +1,20 @@
 from ta_output import ta_with_zips
 from infatuation_output import inf_list
-from michelin_output import michelin_list
+from convert_to_dictionary import mich_dict
 
 def get_names():
     name_list = []
+    filter_list = []
     for i in ta_with_zips:
-         if i['Name'] not in name_list:
-             name_list.append(i['Name'])
+        name_list.append({'Name': i['Name'], 'Address': i['Address'], 'Zip': i['Zip']})
+    for i in name_list:
+        filter_list.append(i['Name'])
     for i in inf_list:
-         if i['Name'] not in name_list:
-             name_list.append(i['Name'])
-
-    for i in michelin_list:
-        if i not in name_list:
-            name_list.append(i)
+         if i['Name'] not in filter_list:
+             name_list.append({'Name': i['Name'], 'Address':' ' , 'Zip': ' '})
+    for i in name_list:
+        filter_list.append(i['Name'])
+    for i in mich_dict:
+        if i['Name'] not in filter_list:
+            name_list.append({'Name': i['Name'], 'Address': ' ',  'Zip': ' '})
     return name_list
