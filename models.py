b41c0ae3be3f0a61bc85514cfcdd6f9edd95b755
kahartman2 <41496627+kahartman2@users.noreply.github.com>
2018-11-13 12:50:11 -0500

    updated models

--- models.py
+++ models.py
@@ -9,42 +9,37 @@ class Restaurant(Base):
 
     id = Column(Integer, primary_key = True)
     name = Column(Text)
-    address = relationship('Address', back_populates = 'restaurant')
-    cuisines = relationship('Cuisine', secondary = 'restaurants_cuisines')
+    address = Column(Text)
+    zip_code = Column(Integer)
+    cuisine = relationship('Cuisine', back_populates = 'restaurant')
     trip_advisor_info = relationship('TripAdvisor', back_populates='restaurant')
     infatuation_info = relationship('Infatuation', back_populates='restaurant')
     michelin_info = relationship('Michelin', back_populates='restaurant')
-
-class Address(Base):
-    __tablename__ = 'address'
-
-    id = Column(Integer, primary_key = True)
-    address = Column(Text)
-    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
-    restaurant = relationship('Restaurant', back_populates='address')
     neighborhood_id = Column(Integer, ForeignKey('neighborhoods.id'))
-    neighborhood = relationship('Neighborhood', back_populates = 'addresses')
+    neighborhood = relationship('Neighborhood', back_populates = 'restaurants')
 
 class Cuisine(Base):
     __tablename__ = 'cuisines'
 
     id = Column(Integer, primary_key = True)
     name = Column(Text)
-    restaurants = relationship('Restaurant', secondary = 'restaurants_cuisines')
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    restaurant = relationship('Restaurant', back_populates='cuisine')
 
 class Neighborhood(Base):
     __tablename__ = 'neighborhoods'
 
     id = Column(Integer, primary_key = True)
     name = Column(Text)
-    addresses = relationship('Restaurant', back_populates = 'neighborhood')
+    zip_code = Column(Integer)
+    restaurants = relationship('Restaurant', back_populates = 'neighborhood')
 
 class TripAdvisor(Base):
     __tablename__ = 'trip_advisor_info'
 
     id = Column(Integer, primary_key = True)
+    price = Column(Integer)
     rating = Column(Integer)
-    price = Column(Text)
     ranking = Column(Integer)
     number_reviews = Column(Integer)
     restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
@@ -54,6 +49,7 @@ class TripAdvisor(Base):
 class TripAdvisorBreakdown(Base):
     __tablename__ = 'trip_advisor_breakdown'
 
+    id = Column(Integer, primary_key = True)
     proportion_one_star = Column(Integer)
     proportion_two_star = Column(Integer)
     proportion_three_star = Column(Integer)
@@ -66,7 +62,7 @@ class Infatuation(Base):
     __tablename__ = 'infatuation_info'
 
     id = Column(Integer, primary_key = True)
-    price = Column(Text)
+    price = Column(Integer)
     rating = Column(Integer)
     restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
     restaurant = relationship('Restaurant', back_populates='infatuation_info')
@@ -75,15 +71,10 @@ class Michelin(Base):
     __tablename__ = 'michelin_info'
 
     id = Column(Integer, primary_key = True)
-    rating = Column(Integer)
+    name = Column(Text)
+    stars = Column(Integer)
     restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
     restaurant = relationship('Restaurant', back_populates='michelin_info')
-
-class RestaurantsCuisines(Base):
-    __tablename__ = 'restaurants_cuisines'
-
-    cuisine_id = Column(Integer, ForeignKey('cuisines.id'), primary_key = True)
-    restaurant_id = Column(Integer, ForeignKey('restaurants.id'), primary_key = True)
 
 engine = create_engine('sqlite:///nyc_restaurants.db')
 Base.metadata.create_all(engine)
